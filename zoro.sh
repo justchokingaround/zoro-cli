fa735967784d24c4f9b89fa3d881ec46d891c071
justchokingaround <ivanonarch@tutanota.com>
2022-07-15 10:05:23 +0200

    refactor: minor cleanup

--- zoro.sh
+++ zoro.sh
@@ -8,25 +8,21 @@ anime_id=$(printf "%s" "$choice"|cut -d"|" -f1|grep -o "[0-9]*$")
 anime_name=$(printf "%s" "$choice" | cut -d"|" -f2)
 
 episodes_list=$(curl -s "https://zoro.to/ajax/v2/episode/list/$anime_id"|grep -Eo 'href=\\"([^\\"]*)\\"'|grep -v "javascript"|sed -En 's_href=\\"(.*)\\"_\1_p')
-# episode_names=$(printf "%s" "$episodes_list"|cut -d'|' -f1)
 episodes_links=$(printf "%s" "$episodes_list"|cut -d'|' -f2)
 
 episodes_number=$(printf "%s\n" "$episodes_links"|wc -l|tr -d "[:space:]")
 
-[ "$episodes_number" -eq 0 ] && printf "No episodes found\n" && exit 1 || [ "$episodes_number" -eq 1 ]
+[ "$episodes_number" -eq 0 ] && printf "No episodes found\n" && exit 1
 [ "$episodes_number" -gt 1 ] && printf "Choose an episode number between 1 and %s: " "$episodes_number" && read -r episode_number
 [ -z "$episode_number" ] && episode_number="$episodes_number"
 
 episode_id=$(printf "%s\n" "$episodes_links"|sed -n "${episode_number}p"|grep -o '[^=]*$')
 source_id=$(curl -s "https://zoro.to/ajax/v2/episode/servers?episodeId=$episode_id"|sed -En 's_data-id=\\"([^"]*)\\".*_\1_p'|grep -o '[^"]*$'|tr -d "[:space:]")
-link=$(curl -s "https://zoro.to/ajax/v2/episode/sources?id=$source_id")
-rapid_link=$(printf "%s" "$link"|sed -En 's_.*"link":"([^"]*)".*_\1_p')
-rapid_id=$(printf "%s" "$link"|sed -En 's_.*"link":"(.*)6/(.*)\?z=".*_\2_p')
+rapid_id=$(curl -s "https://zoro.to/ajax/v2/episode/sources?id=$source_id"|sed -En 's_.*"link":"(.*)6/(.*)\?z=".*_\2_p')
 
 json=$(curl -s "https://rapid-cloud.ru/ajax/embed-6/getSources?id=$rapid_id")
 video_link=$(printf "%s" "$json"|tr "{|}" "\n"|sed -En 's_"file":"([^"]*).*_\1_p'|head -n1)
 subs=$(printf "%s" "$json"|tr "{|}" "\n"|sed -En 's_"file":"([^"]*).*_\1_p'|grep "cc.zorores"|sed 's/:/\\:/g'|tr "\n" ":"|sed 's/:$//')
 
 [ -z "$video_link" ] && printf "No video link found\n" && exit 1
-[ -n "$subs" ] && mpv --sub-files="$subs" --force-media-title="$anime_name - Ep: $episode_number" "$video_link" ||
-  mpv "$video_link" --force-media-title="$anime_name - Ep: $episode_number"
+[ -n "$subs" ] && mpv --sub-files="$subs" --force-media-title="$anime_name - Ep: $episode_number" "$video_link"
