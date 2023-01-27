36c6fbeb0424271921e7a98d91c4ad4fd8ef7c86
justchokingaround <ivanonarch@tutanota.com>
2023-01-27 09:32:56 +0100

    fix: exit if subs exist

--- zoro.sh
+++ zoro.sh
@@ -2,28 +2,34 @@
 
 [ -z "$*" ] && printf "Search an anime: " && read -r query || query=$*
 query=$(printf "%s" "$query" | tr " " "+")
-anime_info=$(curl -s "https://zoro.to/search?keyword=$query"|sed -nE 's_.*href="(.*)\?ref=search" title="([^"]*)".*_\1|\2_p')
-choice=$(printf "%s" "$anime_info"|fzf -d "\|" --with-nth 2..)
-anime_id=$(printf "%s" "$choice"|cut -d"|" -f1|grep -o "[0-9]*$")
+choice=$(curl -s "https://zoro.to/search?keyword=$query" | sed -nE 's_.*href="(.*)\?ref=search" title="([^"]*)".*_\1|\2_p' | fzf -1 -d "\|" --with-nth 2..)
+anime_id=$(printf "%s" "$choice" | sed -nE "s_.*-([0-9]+)\|.*_\1_p")
 anime_name=$(printf "%s" "$choice" | cut -d"|" -f2)
 
-episodes_list=$(curl -s "https://zoro.to/ajax/v2/episode/list/$anime_id"|grep -Eo 'href=\\"([^\\"]*)\\"'|grep -v "javascript"|sed -En 's_href=\\"(.*)\\"_\1_p')
-episodes_links=$(printf "%s" "$episodes_list"|cut -d'|' -f2)
-
-episodes_number=$(printf "%s\n" "$episodes_links"|wc -l|tr -d "[:space:]")
+episodes_links=$(curl -s "https://zoro.to/ajax/v2/episode/list/$anime_id" | tr "<|>" "\n" | sed -nE 's_.*data-id=\\"([0-9]*)\\".*_\1_p')
+episodes_number=$(printf "%s\n" "$episodes_links" | wc -l | tr -d "[:space:]")
 
 [ "$episodes_number" -eq 0 ] && printf "No episodes found\n" && exit 1
 [ "$episodes_number" -gt 1 ] && printf "Choose an episode number between 1 and %s: " "$episodes_number" && read -r episode_number
 [ -z "$episode_number" ] && episode_number="$episodes_number"
 
-episode_id=$(printf "%s\n" "$episodes_links"|sed -n "${episode_number}p"|grep -o '[^=]*$')
-source_id=$(curl -s "https://zoro.to/ajax/v2/episode/servers?episodeId=$episode_id"|sed -En 's_data-id=\\"([^"]*)\\".*_\1_p'|grep -o '[^"]*$'|tr -d "[:space:]")
-rapid_id=$(curl -s "https://zoro.to/ajax/v2/episode/sources?id=$source_id"|sed -En 's_.*"link":"(.*)6/(.*)\?z=".*_\2_p')
+episode_id=$(printf "%s\n" "$episodes_links" | sed -n "${episode_number}p")
+source_id=$(curl -s "https://zoro.to/ajax/v2/episode/servers?episodeId=$episode_id" | tr "<|>" "\n" | sed -nE 's_.*data-id=\\"([^"]*)\\".*_\1_p' | head -1)
+embed_link=$(curl -s "https://zoro.to/ajax/v2/episode/sources?id=$source_id" | sed -nE "s_.*\"link\":\"([^\"]*)\".*_\1_p")
 
-json=$(curl -s "https://rapid-cloud.ru/ajax/embed-6/getSources?id=$rapid_id")
-video_link=$(printf "%s" "$json"|tr "{|}" "\n"|sed -En 's_"file":"([^"]*).*_\1_p'|head -n1)
-subs=$(printf "%s" "$json"|tr "{|}" "\n"|sed -En 's_"file":"([^"]*).*_\1_p'|grep "cc.zorores"|sed 's/:/\\:/g'|tr "\n" ":"|sed 's/:$//')
+# get the juicy links
+parse_embed=$(printf "%s" "$embed_link" | sed -nE "s_(.*)/embed-(4|6)/(.*)\?vast=1\$_\1\t\2\t\3_p")
+provider_link=$(printf "%s" "$parse_embed" | cut -f1)
+source_id=$(printf "%s" "$parse_embed" | cut -f3)
+embed_type=$(printf "%s" "$parse_embed" | cut -f2)
+
+key="$(curl -s "https://github.com/enimax-anime/key/blob/e${embed_type}/key.txt" | sed -nE "s_.*js-file-line\">(.*)<.*_\1_p")"
+json_data=$(curl -s "${provider_link}/ajax/embed-${embed_type}/getSources?id=${source_id}" -H "X-Requested-With: XMLHttpRequest")
+
+video_link=$(printf "%s" "$json_data" | tr "{|}" "\n" | sed -nE "s_.*\"sources\":\"([^\"]*)\".*_\1_p" | base64 -d |
+	openssl enc -aes-256-cbc -d -md md5 -k "$key" 2>/dev/null | sed -nE "s_.*\"file\":\"([^\"]*)\".*_\1_p")
+subs=$(printf "%s" "$json_data" | tr "{|}" "\n" | sed -nE "s_\"file\":\"(.*\.vtt)\".*_\1_p" | sed 's/:/\\:/g' | tr "\n" ":" | sed 's/:$//')
 
 [ -z "$video_link" ] && printf "No video link found\n" && exit 1
-[ -n "$subs" ] && mpv --sub-files="$subs" --force-media-title="$anime_name - Ep: $episode_number" "$video_link"
+[ -n "$subs" ] && mpv --sub-files="$subs" --force-media-title="$anime_name - Ep: $episode_number" "$video_link" && exit 0
 mpv "$video_link" --force-media-title="$anime_name - Ep: $episode_number"
